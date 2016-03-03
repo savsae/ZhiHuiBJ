8a583d12249eedd7a24d49315e331e8ea790e6ea
savsae <savsae@163.com>
2016-03-03 11:07:02 +0800

    Initial commit

---
+++ app/src/main/java/ss/com/zhihuibj/SplashActivity.java
@@ -0,0 +1,19 @@
+package ss.com.zhihuibj;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 闪屏页面
+ */
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+
+}
+
